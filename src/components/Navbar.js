c706cd2455683da4e37d11398956c554d6ec43ef
omar-nav <omar.navarro@g.austincc.edu>
2018-10-27 10:54:21 -0500

    fix paths to use link module

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ class Navbar extends Component {
         <div>
           <ul>
             <li id="logo-container">
-              <a href="https://www.keyence.com.mx/"><img id="logo" src="https://res.cloudinary.com/davd4ynha/image/upload/v1540307121/logo.png"
+              <a href="https://www.keyence.com.mx/" target="_blank" rel="noopener noreferrer"><img id="logo" src="https://res.cloudinary.com/davd4ynha/image/upload/v1540307121/logo.png"
                 alt="Keyence Logo"></img>
               </a>
             </li>
@@ -16,7 +16,7 @@ class Navbar extends Component {
             <li><Link to={`/create`}>Agregar Venta</Link></li>
             <li><Link to={`/line2017`}>Gráfica 2017</Link></li>
             <li><Link to={`/line2018`}>Gráfica 2018</Link></li>
-            <li><a href="https://mapa-de-ventas.herokuapp.com/">Mapa de Ventas</a></li>
+            <li><a href="https://mapa-de-ventas.herokuapp.com/" target="_blank" rel="noopener noreferrer">Mapa de Ventas</a></li>
           </ul>
         </div>
       </nav>
